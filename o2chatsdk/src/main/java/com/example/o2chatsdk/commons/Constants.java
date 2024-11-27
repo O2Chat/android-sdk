6dc97544f96f98985e18e1fc15ca102b8be9bb6e
Umair Abbasi <umair.abbasi@arittek.com>
2024-11-27 13:11:31 +0500

    test server

--- o2chatsdk/src/main/java/com/example/o2chatsdk/commons/Constants.java
+++ o2chatsdk/src/main/java/com/example/o2chatsdk/commons/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
     //CHAT HUB CONNECTION URL
 
-    public static String ChatHubUrl = "https://secure-befiler.o2chat.io/";
-//    public static String ChatHubUrl = "http://175.107.196.226:5003/";
+  //  public static String ChatHubUrl = "https://secure-befiler.o2chat.io/";
+    public static String ChatHubUrl = "https://692a-202-47-59-83.ngrok-free.app/";
    //  public static String ChatHubUrl =  "https://2896-202-47-59-83.ngrok-free.app/";
 
     //SELECTED MENU KEYS OR MENU NAMES
