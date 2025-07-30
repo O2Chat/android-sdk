ff2daf6c903b853b8ddfbf13724bb268f3c36de9
Umair Abbasi <umair.abbasi@arittek.com>
2025-07-30 13:28:18 +0500

    Revert "updated database library"
    This reverts commit d2858d51d8d9b1bc128649f3bfe9bb3e686e55f3.

--- app/src/main/java/com/arittek/signalrtestandroid/localDB/AppDatabase.java
+++ app/src/main/java/com/arittek/signalrtestandroid/localDB/AppDatabase.java
@@ -12,6 +12,8 @@ import com.arittek.signalrtestandroid.localDB.entity.ConversationDetail;
 import com.arittek.signalrtestandroid.localDB.entity.ConversationDetailFile;
 import com.arittek.signalrtestandroid.localDB.entity.NewChatEntity;
 
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SupportFactory;
 
 @Database(entities = {ConversationDetail.class,ConversationDetailFile.class, NewChatEntity.class}, version = 1, exportSchema = false)
 @TypeConverters(DateConverter.class)
@@ -26,19 +28,16 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
             // Initialize SQLCipher with your encryption passphrase
-          //  SQLiteDatabase.loadLibs(context.getApplicationContext());
-    //        String passphrase = "CMitXwKoZFw6PrK";
-  //          byte[] key = SQLiteDatabase.getBytes(passphrase.toCharArray());
+            SQLiteDatabase.loadLibs(context.getApplicationContext());
+            String passphrase = "CMitXwKoZFw6PrK";
+            byte[] key = SQLiteDatabase.getBytes(passphrase.toCharArray());
             // Create a SupportFactory with the encryption key
-//            SupportFactory factory = new SupportFactory(key);
+            SupportFactory factory = new SupportFactory(key);
             // Initialize Room with the encrypted factory
-
-            System.loadLibrary("sqlcipher");
             INSTANCE =
                     Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DATABASE_NAME).allowMainThreadQueries()
                             .build();
-         /*   db = Room.databaseBuilder(context, AppDatabase.class, databaseFile.getAbsolutePath())
-                  .build();*/
+
 
             // allow queries on the main thread.
             // Don't do this on a real app! See PersistenceBasicSample for an example.
